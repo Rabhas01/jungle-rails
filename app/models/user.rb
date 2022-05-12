fdc47097950f19386f07088967e40ab6bf57b55a
Priscilla <rabhas.vashisht@gmail.com>
2022-05-12 12:29:52 -0400

    Final commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   has_secure_password
 
-	# Verify that email field is not blank and that it doesn't already exist in the db (prevents duplicates):
-	validates :email, presence: true, uniqueness: true
+	validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 3}
+	validates :password_confirmation, presence: true, length: { minimum: 3, maximum: 255 }
+	
+  def self.authenticate_with_credentials(email, password)
+    email = email.downcase.strip
+    @user = User.find_by_email(email)
+    if @user && @user.authenticate(password)
+      @user
+    else
+      nil
+    end
+  end
+
+
 end
